c84d65a67f230d6e8032f0411356fcd74effe036
Bubbu Dert <139119578+BubbuDert249@users.noreply.github.com>
2025-07-25 12:21:36 +0200

    Delete openharmony directory

--- openharmony/main.js
+++
@@ -1,4 +0,0 @@
-function switchTab(tab) {
-    document.getElementById('script-tab').classList.toggle('active', tab === 'script');
-    document.getElementById('terminal-tab').classList.toggle('active', tab === 'terminal');
-}
